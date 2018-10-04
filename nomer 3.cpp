a59c532f1f0f9c11bd82c7a33e2a91c5987c2142
BrilliNoval <43838918+BrilliNoval@users.noreply.github.com>
2018-10-04 02:18:24 -0700

    Struktur Data

---
+++ nomer 3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	string kata;
+	cout<<"Masukkan Suatu Kata : ";
+	cin>>kata;
+	cout<<endl;
+	int x=kata.length()-1;
+	for(x;x>=0;x--)
+	{
+		for(int y=0;y<=x;y++){
+			cout<<kata[y];
+		}
+		cout<<endl;
+	}
+	return 0;
+}
